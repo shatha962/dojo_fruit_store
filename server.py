b6d8bd579d3382e445d17d7fe7a3177046263b86
shatha962 <shatha.b96@gmail.com>
2023-05-22 17:18:27 +0300

    dojo fruit store

--- server.py
+++ server.py
@@ -8,7 +8,15 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry = int(request.form['strawberry'])
+    raspberry = int(request.form['raspberry'])
+    apple = int(request.form['apple'])
+    firstName = request.form['first_name']
+    lastName = request.form['last_name']
+    identity = request.form['student_id']
+    count = strawberry+raspberry+apple
+    print(f"Charging {firstName} for {count}")
+    return render_template("checkout.html",strawberry=strawberry, raspberry=raspberry,apple=apple, firstName=firstName, lastName=lastName,identity=identity)
 
 @app.route('/fruits')         
 def fruits():
